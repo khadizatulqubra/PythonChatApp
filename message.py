1ec493bcca3a5d19885db7d9f2d69f24f20c88ad
khadizatulqubra <khadizatul.qubra@chasacademy.se>
2025-02-08 20:21:51 +0100

    insert data in mongo

--- message.py
+++ message.py
@@ -5,7 +5,7 @@ import pymongo
 
 client =pymongo.MongoClient("mongodb://localhost:27017/")
 db= client ["messageapp"]
-message_col=["messages"]
+message_col=db["messages"]
 
 #Tkinter GUI
 root=tk.Tk()
@@ -13,5 +13,21 @@ root.title("messageapp")
 
 enrty=tk.Entry(root,width=40)
 enrty.pack(pady=5)
+
+def send_message():
+    if enrty.get():
+        message_col.insert_one ({"text":enrty.get()})
+        enrty.delete(0,tk.END)
+
+#steg 1 stage code = git add .
+#steg 2 commit code= git commit -m "commit done"
+#steg 3 push code = git push
+
+
+send_button=tk.Button(root,text= "send", command=send_message)
+send_button.pack(pady=5)
+
+messages_label=tk.Label(root, text="Messages:\n" ,justify="left")
+messages_label.pack()
 
 root.mainloop()
